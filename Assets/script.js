bea3b81b36f05a87c5df4efe61e1325942817649
BRASS <mr.brn.lewis@outlook.com>
2021-04-08 14:36:53 +0800

    local storage setup with read/write capability

--- Assets/script.js
+++ Assets/script.js
@@ -1,27 +1,68 @@
+/*using vanilla JS to manage the element manipulation rather than jQuery (for learning purposes only)*/
 
-
-/*add a div elements to container*/
-
-/*using vanilla JS to create div element with classes*/
-
+/*get elements by ID */
 var plannerContainer = document.getElementById("time-block-container");
-var hourCol =  "<div class='col-2 hours' ></div>";
-var inputCol = "<div class='col-8 description' ><textarea> </textarea></div>";
-var saveCol =  "<div class='btn col-2 saveBtn' ></div>";
-var plannerRow = "<div class='row time-block'>" + hourCol + inputCol + saveCol + "</div>";
+var tDate = document.getElementById("currentDay");
 
+/*using moment to get the date*/
+tDate.innerHTML = moment().format("dddd, MMMM Do");
+
+/*creating some HTML for row and col divs*/
+var hourCol =  "<div class='col-2 hour' ></div>";
+var inputCol = "<div class='col-9'><textarea class='description'> </textarea></div>";
+var saveCol =  "<button class='col-1 saveBtn' > <i class='far fa-save'> </i> </button>";
+var plannerRow = "<div class='row time-block'>" + hourCol + inputCol + saveCol + "</div>";
 
 for (var i = 9; i < 18; i++) 
 {
     plannerContainer.innerHTML +=plannerRow; /* write plannerRow to the plannerContainer */
 }
 
-/* create object collection (array?) of the plannerRows */
+var plannerUpate = document.getElementsByTagName("button");
 
-
-
+/*calling some functions*/
 colorAssignment(9);
 hourAssignment(9);
+
+/*add eventlistner to buttons this is really cool way of managing..*/
+
+const cbox = document.querySelectorAll(".saveBtn");
+const ctext = document.querySelectorAll(".description")
+
+
+
+ for (let i = 0; i < cbox.length; i++) 
+ {
+     cbox[i].addEventListener("click", function(event) 
+     {
+        
+        let dataToSave = ctext[i].value
+        if(dataToSave != " ") //seems to be the default string of these text areas
+
+        {
+        localStorage.setItem(i,dataToSave);
+        console.log(localStorage)
+        }
+        event.stopPropagation();
+     });
+}
+
+function readPlannerData()
+{
+    let dayEvent;
+
+    for (let i = 0; i < 9; i++) 
+    {
+       dayEvent = localStorage.getItem(i)
+        if(dayEvent != null) 
+        {
+            ctext[i].value = dayEvent;
+        }
+    }
+}
+
+readPlannerData();
+
 function colorAssignment(sHour)
 {
     var id = plannerContainer.children;
@@ -47,7 +88,7 @@ function colorAssignment(sHour)
 
 function hourAssignment(sHour)
 {
-    var id = document.getElementsByClassName("hours");
+    var id = document.getElementsByClassName("hour");
 
     for(i=0; i<id.length; i++)
     {
@@ -61,7 +102,6 @@ function hourAssignment(sHour)
         {
             id[i].innerHTML = "<p>" + (i+sHour)  + "PM</p>";
         }
-
     }
-
 }
+
